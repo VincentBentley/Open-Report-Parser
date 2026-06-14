1494447df473486c255c65a555dc0ed8a372b581
Vincent Bentley <vince@vincentbentley.co.uk>
2026-06-14 18:14:02 +0100

    Applied recommended fix for issue #36

--- report-parser.pl
+++ report-parser.pl
@@ -3,6 +3,7 @@
 # -----------------------------------------------------------------------------
 #
 # Open Report Parser - Open Source report parser
+# Copyright (C) 2026 Vincent Bentley
 # Copyright (C) 2023 John Bradley (userjack6880)
 # Copyright (C) 2016 TechSneeze.com
 # Copyright (C) 2012 John Bieling
@@ -11,7 +12,9 @@
 #   main script
 #
 # Available at: https://github.com/userjack6880/Open-Report-Parser
+#      Fork at: https://github.com/VincentBentley/Open-Report-Parser
 #
+# 14JUN26: Applied fix for issue 36 - IPv6
 # -----------------------------------------------------------------------------
 #
 #  This file is part of Open Report Parser.
@@ -1203,7 +1206,7 @@ sub storeXMLInDatabase {
       $iptype = "ip";
     } 
     elsif($nip = inet_pton(AF_INET6, $ip)) {
-      $ipval = $dbx{to_hex_string}($nip);
+      $ipval = $dbx{to_hex_string}($nip); # 14JUN26 Issue 36
       $iptype = "ip6";
     } 
     else {
@@ -1646,7 +1649,7 @@ sub storeJSONInDatabase {
         $iptype = "ip";
       }
       elsif($nip = inet_pton(AF_INET6, $ip)) {
-        $ipval = $dbx{to_hex_string}{$nip};
+        $ipval = $dbx{to_hex_string}($nip);
         $iptype = "ip6";
       }
       else {
